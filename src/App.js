426148e331d260b547b2c6ed37ef3fd144bb710d
Sujan Thapa <sujan.st98644@gmail.com>
2024-06-06 18:56:16 +0545

    added menus

--- src/App.js
+++ src/App.js
@@ -16,14 +16,15 @@ function App() {
     }
   },[Scrolled])
   return (
-    <main className="flex flex-col font-playfair">
-        <header className={`grid lg:grid-cols-3 items-center bg-green-700 text-white pl-8 transition-colors duration-300 ${
-          Scrolled ? "bg-white text-black" : "bg-green-700 text-white"} fixed top-0 left-0 w-full`}>
-          <div>
-            <img src="logo192.png" alt="Logo" className=" h-14" />
+    <main className="flex flex-col font-sora text-white bg-[#EEF7FF]">
+        <header className={`grid lg:grid-cols-3 items-center bg-green-700  pl-8 transition-colors duration-300 ${
+          Scrolled ? "bg-white text-black" : ""} fixed top-0 left-0 w-full`}>
+          <div className="flex flex-row items-center font-sora font-bold">
+            <img src="Burger.png" alt="logo" className="h-14" />
+            Chrunchy
           </div>
           <div className="font-sora col-start-3 justify-end">
-            <ul className="flex flex-row space-x-4 ">
+            <ul className="flex flex-row space-x-10 ">
               <a href="#Home">Menu</a>
               <a href="#blogs">Order</a>
               <a href="#pages">Packs</a>
@@ -31,8 +32,18 @@ function App() {
             </ul>
           </div>
         </header>
-      <div className=" h-screen bg-[#EEF7FF] font-semibold text-9xl">
-        
+      <div className=" h-full font-semibold grid grid-cols-4 grid-rows-3 gap-4 mx-5 mt-16">
+          {/* <div className="shadow-xl rounded-xl h-72 bg-cover"
+            style={{ backgroundImage: "url('Momo.jpg')" }} >
+          <p className="text-white text-3xl m-3">Momo</p>
+          </div> */}
+      <img src="Momo.jpg" alt="quisines" className="shadow-xl rounded-xl"/>
+      <img src="Thukpa.jpg" alt="quisines" className="shadow-xl rounded-xl" />
+      <img src="iron.jpg" alt="quisines" className=" col-span-2 row-span-2 shadow-xl rounded-xl" />
+      <img src="Sekuwa.jpg" alt="quisines" className=" shadow-xl rounded-xl" />
+      <img src="Thakali.jpg" alt="quisines" className=" shadow-xl rounded-xl" /> 
+      <img src="Samai.jpg" alt="quisines" className="shadow-xl rounded-xl col-span-1" />
+      <img src="background.jpg" alt="quisines" className="shadow-xl rounded-xl col-span-2" />
       </div>
     </main>
   );
